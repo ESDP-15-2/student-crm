d29e584916f7f69e96e87e76dc3e967e325fb19e
Marat Marat <kulbaev.mart@gmail.com>
2015-11-16 15:59:45 +0600

    #1 Добавлен гем Faker, и созданы фикстуры

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ror_course = Course.create(name: 'ROR')
+
+groups = []
+groups.push Group.create(name: 'GR#1',
+                         course: ror_course
+            )
+groups.push Group.create(name: 'GR#2',
+                         course: ror_course
+            )
+students = []
+40.times do
+  student_name = Faker::Name.name
+  students.push Student.create(name: student_name,
+                               phone: Faker::PhoneNumber.cell_phone,
+                               email: Faker::Internet.email(student_name),
+                )
+end
+
+group_memberships = []
+40.times do
+  group_memberships.push GroupMembership.create(student: students.sample,
+                                                group: groups.sample,
+                                                active: true)
+end
