1df812df6d6ba855d1b2e0e0c9bec5c1cedd70a0
aliia_kaiumova <aliia.kaiumova@gmail.com>
2015-11-18 01:03:26 +0600

    #2  В контроллере "contact_lists" созданы экшены для создания, просмотра и удаления списка получателей. В application.html.erb добавлено отображение flash-сообщений.

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -1,2 +1,37 @@
 class ContactListsController < ApplicationController
+  def index
+    @contact_lists = ContactList.all
+  end
+
+  def show
+    @contact_list = ContactList.find(params[:id])
+  end
+
+  def new
+    @contact_list = ContactList.new
+  end
+
+  def create
+    @contact_list = ContactList.new(contact_list_params)
+    if @contact_list.save
+      redirect_to contact_lists_path
+      flash[:success] = '?????? ??????????? ??????? ??????'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @contact_list = ContactList.find(params[:id])
+    @contact_list.destroy
+    flash[:success] = '?????? ??????????? ??????'
+
+    redirect_to contact_lists_path
+  end
+
+  private
+
+  def contact_list_params
+    params.require(:contact_list).permit(:title, student_ids: [])
+  end
 end
