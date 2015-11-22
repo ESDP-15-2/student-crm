24597f1ba4345294f657a91c2fb84d35d598a470
Marat Marat <kulbaev.mart@gmail.com>
2015-11-22 15:16:05 +0600

    #3 в index.html.erb и show.html.erb добавлены кнопки 'отправить', кнопка отправляет сообщение по номерам из списков получателей. Добавлен post запрос, который отправляет сгенерированный xml на сервис 'nikita'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'sms_deliveries/:id/send' => 'sms_deliveries#send_message', as: 'sms_send_message'
+
   root 'sms_deliveries#root'
 
   resources :contact_lists, :sms_deliveries, :students
