23e2b6518ef9fc401f5091976337ca7433979ae6
Marat Marat <kulbaev.mart@gmail.com>
2015-11-22 15:16:55 +0600

    #3 Изменен seeds.rb добвлены контакнты нашей команды и гудова

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,55 @@
 
 ror_course = Course.create(name: 'ROR')
 
-groups = []
-groups.push Group.create(name: 'GR#1',
-                         course: ror_course)
-groups.push Group.create(name: 'GR#2',
-                         course: ror_course)
-students = []
-40.times do
-  student_name = Faker::Name.name
-  students.push Student.create(name: student_name,
-                               phone: Faker::PhoneNumber.cell_phone,
-                               email: Faker::Internet.email(student_name))
-end
+# groups = []
+# groups.push Group.create(name: 'GR#1',
+#                          course: ror_course)
+# groups.push Group.create(name: 'GR#2',
+#                          course: ror_course)
+# students = []
+# 40.times do
+#   student_name = Faker::Name.name
+#   students.push Student.create(name: student_name,
+#                                phone: Faker::PhoneNumber.cell_phone,
+#                                email: Faker::Internet.email(student_name))
+# end
 
-group_memberships = []
 
-counter = 0
+abai = Student.create!(name:'Abai Ismail',
+                       phone:'996550113377',
+                       email: 'aismai@exmple.com')
 
-40.times do
-  group_memberships.push GroupMembership.create(student: students[counter],
-                                                group: groups.sample,
-                                                active: true)
-  counter += 1
-end
+aliia = Student.create!(name:'Aliia Kaiumova',
+                        phone:'996700605070',
+                        email: 'tumanskaya@exmple.com')
+
+marat = Student.create!(name:'Marat Kulbaev',
+                        phone:'996555623310',
+                        email: 'klmart@exmple.com')
+
+mahabat = Student.create!(name:'Mahabat',
+                          phone:'996700067042',
+                          email: 'mahabbat@exmple.com')
+
+sanjar = Student.create!(name:'Sanjar Musaev',
+                         phone:'996550612056',
+                         email: 'aismai@exmple.com')
+
+gudov = Student.create!(name:'Aleksandr Gudov',
+                         phone:'996707861277',
+                         email: 'gudov@exmple.com')
+
+# group_memberships = []
+#
+# counter = 0
+#
+# 40.times do
+#   group_memberships.push GroupMembership.create(student: students[counter],
+#                                                 group: groups.sample,
+#                                                 active: true)
+#   counter += 1
+# end
+
+# contact_lists = []
+#     contact_lists.push ContactList.create(title: 'ROR GR#1')
+#     contact_lists.push ContactList.create(title: 'ROR GR#2')
