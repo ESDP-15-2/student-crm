493481bbffcfcc345232c7573ff17c343f8db647
= <=>
2015-11-26 14:45:20 +0600

    #15 Установлена связь один ко многим между моделями sms_service_account и sender

---
+++ app/models/sender.rb
@@ -0,0 +1,8 @@
+class Sender < ActiveRecord::Base
+
+  belongs_to :sms_service_account
+  has_many :sms_deliveries
+
+  validates :name, presence: true, length: {maximum: 14}
+
+end
--- app/models/sms_service_account.rb
+++ app/models/sms_service_account.rb
@@ -1,4 +1,9 @@
 class SmsServiceAccount < ActiveRecord::Base
+
+  has_many :senders
+  accepts_nested_attributes_for :senders, reject_if: :all_blank, allow_destroy: true
+
   validates :login, presence: true, uniqueness: true
   validates :password, presence: true
+
 end
