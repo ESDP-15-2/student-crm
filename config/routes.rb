f7013ea56cbd8f1d12dd74b3d571a46c34ec213a
= <=>
2015-11-26 14:57:37 +0600

    #15 Добавлен ресурсный роутинг для контроллера sms_service_accounts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'sms_deliveries#index'
 
   resources :students
   resources :contact_lists
   resources :sms_deliveries
+  resources :sms_service_accounts
+
   post 'sms_deliveries/:id/send' => 'sms_deliveries#send_message', as: 'sms_send_message'
 end
