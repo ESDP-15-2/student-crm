f95a21e23053ad760daca40d3f7cc5892996d163
Sanzhar Musaev <otakusan777@gmail.com>
2015-11-27 14:53:11 +0600

    Удалена ненужная валидация на имя автора

--- app/models/sms_delivery.rb
+++ app/models/sms_delivery.rb
@@ -9,9 +9,6 @@ class SmsDelivery < ActiveRecord::Base
   validates :title, presence: true,
             length: { maximum: 50 }
 
-  validates :author, presence: true,
-            length: { maximum: 50 }
-
   validates :content, presence: true,
             length: { maximum: 800 }
 
