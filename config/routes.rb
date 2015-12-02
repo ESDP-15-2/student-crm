18fd70a2e05ae69bf77b83c759ab64c33918860c
Marat Marat <kulbaev.mart@gmail.com>
2015-12-02 10:32:00 +0600

    #14 Добавлен get запрос для метода new_from_contact_list

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   get 'select_objects/select_group/:id' => 'select_objects#select_group', as: 'select_groups'
   get 'select_objects/select_students/:id' => 'select_objects#select_students', as: 'select_students'
+  get 'sms_deliveries/new_from_contact_list/:id' => 'sms_deliveries#new_from_contact_list', as: 'sms_new_from_contact_list'
+
 end
