f47cdacbd4716d109af411649884d15c6dcd3393
aliia_kaiumova <aliia.kaiumova@gmail.com>
2015-12-11 22:32:15 +0600

    #26 Исправлены связи в моделях Group и ContactList

--- app/models/contact_list.rb
+++ app/models/contact_list.rb
@@ -1,8 +1,8 @@
 class ContactList < ActiveRecord::Base
   has_many :recipient_depositories
-  has_many :students, through: :recipient_depositories
+  has_many :users, through: :recipient_depositories
   has_many :sms_deliveries, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true, length: { maximum: 50 }
-  validates :students, presence: true
+  validates :users, presence: true
 end
