d736a9fdda0eccdd865fb468504989ef20a83e04
aliia_kaiumova <aliia.kaiumova@gmail.com>
2015-12-11 14:17:02 +0600

    #27 Сгенерирован контроллер users, добавлены экшены для создания, редактирования, просмотра и удаления пользователя

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210105257) do
+ActiveRecord::Schema.define(version: 20151211072029) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "user_id"
@@ -99,6 +99,17 @@ ActiveRecord::Schema.define(version: 20151210105257) do
   add_index "recipient_depositories", ["contact_list_id"], name: "index_recipient_depositories_on_contact_list_id"
   add_index "recipient_depositories", ["user_id"], name: "index_recipient_depositories_on_user_id"
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
+
   create_table "senders", force: :cascade do |t|
     t.string   "name"
     t.integer  "sms_service_account_id"
@@ -151,5 +162,12 @@ ActiveRecord::Schema.define(version: 20151210105257) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
 end
