bf4b29b2c0b1db18a24281bcbc983965173d5bf3
aliia_kaiumova <aliia.kaiumova@gmail.com>
2015-12-16 14:14:26 +0600

    #28 Изменен файл ability.rb
    Прописаны возможности и ограничения для следующих ролей: администратор, пользователь с полным доступом и студенты

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    if user.has_role? :admin
+      can :manage, :all
+    elsif user.has_role? :user_with_full_authority
+      can :manage, :all
+    elsif user.has_role? :student
+      can [:show, :update ], User, :id => user.id
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
