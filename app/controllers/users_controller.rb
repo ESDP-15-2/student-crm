e28fa8c4ac3405db8337a36e532798e85f282bfd
aliia_kaiumova <aliia.kaiumova@gmail.com>
2015-12-17 16:10:54 +0600

    #27 Изменен файл users_controller.rb
    * В разрешенные параметры добавлены роли
    * Добавлен метод для присваивания ролей пользователю

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   add_breadcrumb 'Пользователи', :users_url
 
- def show
+  def show
     @user = User.find(params[:id])
   end
 
@@ -14,18 +14,20 @@ class UsersController < ApplicationController
   def students
     add_breadcrumb 'Студенты', :students_url
 
-    student = Role.find_by(name: 'student')
+    student = Role.find_by(name: 'Студент')
     @students = student.users.paginate(page: params[:page], per_page: 10)
   end
 
   def new
+    add_breadcrumb 'Создание пользователя', :new_user_url
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    add_roles(@user)
 
-    if user.save
+    if @user.save
       flash[:success] = 'Пользователь успешно создан'
       redirect_to users_url
     else
@@ -35,11 +37,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    add_breadcrumb 'Редактирование данных пользователя', :new_user_url
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
+    add_roles(@user)
 
     if @user.update(user_params)
       redirect_to users_url
@@ -48,7 +52,7 @@ class UsersController < ApplicationController
       flash[:danger] = 'Вы ввели некорректные данные, проверьте и попробуйте снова'
       render 'edit'
     end
-    end
+  end
 
   def destroy
     @user = User.find(params[:id])
@@ -61,7 +65,14 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :surname, :birthdate, :gender, :passport_data, :email, :password,
-                                 contact_attributes: [:phone, :additional_phone, :skype], :role_ids => [])
+    params.require(:user).permit(:name, :surname, :birthdate, :gender, :passport_data, :email, :password, group_ids: [], role_ids: [],
+                                 contact_attributes: [:id, :phone, :additional_phone, :skype])
+  end
+
+  def add_roles(user)
+    user.roles = []
+    params[:user][:role_ids].each do |r|
+      user.roles.push(Role.find(r)) unless r.blank?
+    end
   end
 end
