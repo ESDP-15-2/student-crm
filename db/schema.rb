1f7b1e0c9b1ed6523a6b820a0de74abaf40cd1b7
aismai <abai.ismailov@gmail.com>
2015-12-26 14:19:35 +0600

    #38 добавлены методы для получения номеров из файла

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226080418) do
+ActiveRecord::Schema.define(version: 20151226081244) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "user_id"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20151226080418) do
 
   create_table "contact_lists", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "temp",       default: false
   end
 
   create_table "contacts", force: :cascade do |t|
