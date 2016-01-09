60cd02f74dea772e4c373f674abe91302d4ddbb7
aliia_kaiumova <aliia.kaiumova@gmail.com>
2016-01-10 00:14:30 +0600

    #36 Изменено оформление во вьюшках для рассылок

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -2,6 +2,5 @@ class Sender < ActiveRecord::Base
   belongs_to :sms_service_account
   has_many :sms_deliveries
 
-  validates :name, presence: true, length: {maximum: 14}
-  # validates :sms_service_account, presence: true
+  # validates :name, presence: true, length: {maximum: 14}
 end
