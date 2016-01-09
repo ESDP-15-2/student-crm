8241b48b7a5b38fae36cc27b02052e8b0d8ad8eb
aliia_kaiumova <aliia.kaiumova@gmail.com>
2016-01-09 21:29:15 +0600

    #36 Изменено оформление во вьюшках для листов получателей

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -1,18 +1,18 @@
 class ContactListsController < ApplicationController
 
-  add_breadcrumb 'Листы Получателей', :contact_lists_url
+  add_breadcrumb 'Листы получателей', :contact_lists_url
   def index
     @contact_lists = ContactList.all
   end
 
   def show
     @contact_list = ContactList.find(params[:id])
-    add_breadcrumb @contact_list.title, :contact_list_url
+    add_breadcrumb 'Просмотр листа получателей', :contact_list_url
   end
 
   def new
     @contact_list = ContactList.new
-    add_breadcrumb 'Новый Лист', :new_contact_list_url
+    add_breadcrumb 'Новый лист', :new_contact_list_url
   end
 
   def create
@@ -28,7 +28,7 @@ class ContactListsController < ApplicationController
 
   def edit
     @contact_list = ContactList.find(params[:id])
-    add_breadcrumb 'Редактировать'
+    add_breadcrumb 'Редактирование листа получателей'
   end
 
   def update
