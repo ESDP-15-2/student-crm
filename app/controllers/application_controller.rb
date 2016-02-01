3483cf552ebf292c2fb972513c5d11a33eee8f60
Sanzhar Musaev <otakusan777@gmail.com>
2016-02-01 15:01:54 +0600

    # 48 Изменена структура кода
    * Изменен порядок в routes
    * Изменен application_controller, добавлен запрос авторизации для всех унаследованных контроллеров, удалены лишние методы
    * Удален ненужный view-файл

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,38 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  layout :resolve_layout
+
+  before_action :authenticate_user!
 
   private
 
-  def resolve_layout
-    if devise_controller? && resource_name == :user && action_name == 'new'
-        'sign_in'
-      else
-        'application'
-    end
-  end
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :surname, :gender, :birthdate,
-                                                            :passport_data, :email, :password, :password_confirmation,
-                                                            contact_attributes:[:phone, :additional_phone, :skype]) }
+    devise_parameter_sanitizer.for(:sign_up) do |u| u.permit(:name,
+                                                             :surname,
+                                                             :gender,
+                                                             :birthdate,
+                                                             :passport_data,
+                                                             :email,
+                                                             :password,
+                                                             :password_confirmation,
+                                                             contact_attributes:[:phone,
+                                                                                 :additional_phone,
+                                                                                 :skype])
+    end
 
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :surname, :gender, :birthdate,
-                                                            :passport_data, :email, :password, :password_confirmation,
-                                                            :current_password,
-                                                            contact_attributes:[:phone, :additional_phone, :skype]) }
+    devise_parameter_sanitizer.for(:account_update) do |u| u.permit(:name,
+                                                                   :surname,
+                                                                   :gender,
+                                                                   :birthdate,
+                                                                   :passport_data,
+                                                                   :email,
+                                                                   :password,
+                                                                   :password_confirmation,
+                                                                   :current_password,
+                                                                   contact_attributes:[:phone,
+                                                                                       :additional_phone,
+                                                                                       :skype])
+    end
   end
 
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -11,32 +11,32 @@ Rails.application.routes.draw do
     end
   end
 
-
+  resources :users
   get 'users/:id/profile' => 'users#profile', as: 'user_profile'
   get 'users/students' => 'users#students', as: 'students'
   get 'users/:id/show' => 'users#show', as: 'show_user'
 
 
-  resources :users,
-            :contact_lists,
-            :sms_deliveries,
-            :sms_service_accounts,
-            :senders,
-            :course_elements,
+  resources :course_elements,
             :courses,
             :groups,
 						:periods
+
+  resources :sms_deliveries,
+            :contact_lists,
+            :sms_service_accounts,
+            :senders
+
+  get 'sms_deliveries/new_from_contact_list/:id' => 'sms_deliveries#new_from_contact_list', as: 'sms_new_from_contact_list'
+  get 'sms_deliveries/resend_message/:id' => 'sms_deliveries#resend_message', as: 'sms_resend'
+  post 'sms_deliveries/:id/send' => 'sms_deliveries#send_message', as: 'sms_send_message'
 
   resources :custom_lists do
     collection {post :import}
   end
 
-
-  post 'sms_deliveries/:id/send' => 'sms_deliveries#send_message', as: 'sms_send_message'
   get 'select_objects/select_group/:id' => 'select_objects#select_group', as: 'select_groups'
   get 'select_objects/select_students/:id' => 'select_objects#select_students', as: 'select_students'
-  get 'sms_deliveries/new_from_contact_list/:id' => 'sms_deliveries#new_from_contact_list', as: 'sms_new_from_contact_list'
-  get 'sms_deliveries/resend_message/:id' => 'sms_deliveries#resend_message', as: 'sms_resend'
-	get '/calendar' => 'periods#calendar', as: 'calendar'
 
+	get '/calendar' => 'periods#calendar', as: 'calendar'
 end
