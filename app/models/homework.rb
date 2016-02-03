1f1801d4dcb62ae1297793be1c10ee90aeda5b46
Marat Marat <kulbaev.mart@gmail.com>
2016-02-03 14:37:12 +0600

    #46 добавлена свзять many to one можеду ДЗ и периодами

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,4 +1,5 @@
 class Homework < ActiveRecord::Base
+  belongs_to :period
   has_attached_file :hw_archive
   validates_attachment_content_type :hw_archive,
                                     content_type: ['application/zip', 'application/x-rar-compressed']
--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,6 @@
 class Period < ActiveRecord::Base
   belongs_to :course_element
   has_many :attendances
+  has_many :homeworks
   has_many :students, through: :attendances
 end
