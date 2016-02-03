b65d4515df7b531b5dd6cc7ce35c1cf5de571d77
Marat Marat <kulbaev.mart@gmail.com>
2016-02-03 14:53:24 +0600

    #46 в homeworks_controller добавлен методы которые вытаскивают курс группу и задют новое имя для hw_archive_file_name

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -5,6 +5,7 @@ class HomeworksController < ApplicationController
 
   def create
     @homework = Homework.new(homework_params)
+    set_homework_name
     @homework.user = current_user
 
     if @homework.save
@@ -41,6 +42,25 @@ class HomeworksController < ApplicationController
   end
 
   private
+
+  def get_course_group_name
+    group_name = ''
+    course_name = ''
+    current_user.groups.each do |g|
+      group_name = g.name.to_s
+      course_name = g.course.name.to_s
+    end
+    return course_name, group_name
+  end
+
+  def set_homework_name
+    name = current_user.name.to_s
+    surname = current_user.surname.to_s
+    course_group_name = get_course_group_name.join('-')
+    period_tittle = @homework.period.title.to_s
+    hw_archive_name = name + "-#{surname}" + "-#{course_group_name}" + "-#{period_tittle}"
+    @homework.hw_archive_file_name = hw_archive_name
+  end
 
   def homework_params
     params.require(:homework).permit( :rating, :review, :hw_archive, :period_id, :user_id)
