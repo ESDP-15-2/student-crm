8e17c001103790da037b182bf89c95f25cadafdb
klmart <kulbaev.mart@gmail.com>
2016-02-17 02:22:39 +0600

    49 Создана методы и вьюшки для редактирования модуля

--- app/controllers/academic_units_controller.rb
+++ app/controllers/academic_units_controller.rb
@@ -6,20 +6,37 @@ class AcademicUnitsController < ApplicationController
     @academic_unit = AcademicUnit.new
   end
   def create
-    @academic_unit = AcademicUnit.new(academic_umit_params)
+    @academic_unit = AcademicUnit.new(academic_unit_params)
     if @academic_unit.save
       redirect_to academic_units_url
     else
       render new
     end
   end
+
   def show
     @academic_unit = AcademicUnit.find(params[:id])
   end
-  
+  def edit
+    @academic_unit = AcademicUnit.find(params[:id])
+  end
+
+  def update
+    def update
+      @academic_unit = AcademicUnit.find(params[:id])
+
+      if @academic_unit.update(academic_unit_params)
+        flash[:success] = 'Учебный модульобновлен'
+        redirect_to academic_units_url
+      else
+        flash[:danger] = 'Вы ввели некорректные данные, проверьте и попробуйте снова'
+        render 'edit'
+      end
+    end
+  end
 
   private
-  def academic_umit_params
+  def academic_unit_params
     params.require(:academic_unit).permit(:title, :starts_at, :ends_at,  period_ids: [])
   end
 end
