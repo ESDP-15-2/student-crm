d6b0c47f7509dfb02298f5cc6cdd3ae86de70070
klmart <kulbaev.mart@gmail.com>
2016-02-16 16:50:53 +0600

    #48 добавлены методы для фильтрации периодов

--- app/controllers/student_offices_controller.rb
+++ app/controllers/student_offices_controller.rb
@@ -1,19 +1,37 @@
 class StudentOfficesController < ApplicationController
-  def all_periods
-    @periods = Period.all
-    @homeworks = Homework.all
-    @user_hws = current_user_homeworks
 
+  def get_course_group
+    group = ''
+    course = ''
+    current_user.groups.each do |g|
+      group = g.id
+      course = g.id
+    end
+    return course, group
   end
 
-  def current_user_homeworks
-    @user_hws = []
-    @homeworks.each do |hw|
-     if hw.user == current_user
-       @user_hws.push hw
-     end
+  def array_periods
+    array_periods = []
+    periods = Period.where(hw_status: true, group_id: get_course_group[0])
+    periods.each do |period|
+      if period.commence_datetime < Time.now
+        array_periods.push period
+      end
     end
-    return @user_hws
+    return array_periods
+  end
+
+  def user_home_periods
+    user_home_periods = []
+    @user_hws = Homework.where(user_id: current_user.id)
+    @user_hws.each do |hw|
+      user_home_periods.push hw.period
+    end
+    return user_home_periods
+  end
+
+  def all_periods
+    @periods = array_periods - user_home_periods
   end
 
   def show_one_period
