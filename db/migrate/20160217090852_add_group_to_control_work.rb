4383c28706043e03aed592be85b0de6e5c2dbaa0
klmart <kulbaev.mart@gmail.com>
2016-02-17 15:22:17 +0600

    51 добавлены миграции для контрольной

--- db/migrate/20160217090852_add_group_to_control_work.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupToControlWork < ActiveRecord::Migration
-  def change
-    add_reference :control_works, :group, index: true, foreign_key: true
-  end
-end
--- db/migrate/20160217091039_add_course_to_control_work.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseToControlWork < ActiveRecord::Migration
-  def change
-    add_reference :control_works, :course, index: true, foreign_key: true
-  end
-end
